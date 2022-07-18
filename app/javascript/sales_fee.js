451210b9375472185c713325200720d79ac0233a
osamuccho <basket.o.y@icloud.com>
2022-07-18 20:19:11 +0900

    販売手数料実装機能修正

--- app/javascript/sales_fee.js
+++ app/javascript/sales_fee.js
@@ -1,12 +1,9 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
- //  console.log(priceInput)
-   priceInput.addEventListener("input", () => {
+  priceInput.addEventListener("input", () => {
      const addTaxDom = document.getElementById("sales-fee");
-     // console.log(priceInput.innerHTML);
      addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
      const addPofitDom = document.getElementById("profit");
-     // console.log(priceInput.innerHTML);
-     addPofitDom.innerHTML = Math.floor(priceInput.value - Math.round(priceInput.value * 0.1 ))
+     addPofitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
  })
 });
